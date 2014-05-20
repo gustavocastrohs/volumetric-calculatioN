5f16f5033e4aee8c3f6145274eb115333a0432c5
gustavocastrohs <gustavo.henrique@acad.pucrs.br>
2014-05-20 19:28:00 -0300

    OK

--- volumetric-calculatioN/src/viewcontroller/EstimationForm.java
+++ volumetric-calculatioN/src/viewcontroller/EstimationForm.java
@@ -110,9 +110,16 @@ public class EstimationForm extends javax.swing.JFrame {
                 "Nome", "Tipo", "Tamanho", "Precisão", "Nulo", "Tamanho Medio", "% Linhas Nulas"
             }
         ) {
+            Class[] types = new Class [] {
+                java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.String.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class
+            };
             boolean[] canEdit = new boolean [] {
                 false, false, false, false, false, true, true
             };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
                 return canEdit [columnIndex];
